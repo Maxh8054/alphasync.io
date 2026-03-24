809aed03576ceb6231fa6e1796397c28d924747b
Maxh8054 <maxh8054@OUTLOOK.COM.BR>
2026-03-24 18:26:07 -0300

    serverless: api/dump + api/salva

--- salva.php
+++
@@ -1,8 +0,0 @@
-<?php
-header("Access-Control-Allow-Origin: *");
-$f="dump.json";
-$j=json_decode(file_get_contents($f),1)??[];
-if(count($_FILES)){$n=uniqid().".jpg";move_uploaded_file($_FILES["selfie"]["tmp_name"],$n);$j[]=["t"=>"selfie","v"=>$n];}
-else{$p=json_decode(file_get_contents("php://input"),1);if($p)$j[]=$p;}
-file_put_contents($f,json_encode($j,JSON_UNESCAPED_UNICODE));
-?>
